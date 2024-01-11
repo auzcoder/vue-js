6d3593067c4e89346fd676f04bf3670aa3b56ad0
Abdulkhafiz Davlatov <auz.offical@gmail.com>
2024-01-11 18:29:49 +0500

    .

--- src/compponent/movie-list/MovieList.vue
+++ src/compponent/movie-list/MovieList.vue
@@ -3,9 +3,19 @@
 </script>
 
 <template>
-
+  <ul class="movie-list list-group">
+    <li>Sayrga chiqgan go'dak</li>
+    <li>Poyitaxt - Sulton Abdulhamidxon</li>
+    <li>Transformerlar - 2</li>
+  </ul>
 </template>
 
 <style scoped>
-
+.movie-list {
+  margin-bottom: 2rem;
+  padding: 1.5rem;
+  background-color: #fcfaf5;
+  border-radius: 4px;
+  box-shadow: 15px 15px 15px rgba(0, 0, 0, 0.15);
+}
 </style>
