1b9e192998b3a8fcdde15c7ff3ea7c59b7d4d79d
Abdulkhafiz Davlatov <auz.offical@gmail.com>
2024-01-11 20:34:01 +0500

    .

--- src/compponent/movie-list-item/MovieListItem.vue
+++ src/compponent/movie-list-item/MovieListItem.vue
@@ -4,7 +4,18 @@
 
 <template>
   <li class="list-group-item d-flex justify-content-between">
+    <span class="list-group-item-label">Poyitaxt - Sulton Abdulhamidxon</span>
+    <input type="number" class="list-group-item-input" defaultValue="555" />
+    <div class="d-flex justify-content-center align-items-center">
+      <button class="btn-cookie btn-sm" type="button">
+        <i class="fas fa-cookie"></i>
+      </button>
 
+      <button class="btn-trash btn-sm" type="button">
+        <i class="fas fa-trash"></i>
+      </button>
+      <i class="fas fa-star"></i>
+    </div>
   </li>
 <!--  <li>Sayrga chiqgan go'dak</li>-->
 <!--  <li>Poyitaxt - Sulton Abdulhamidxon</li>-->
